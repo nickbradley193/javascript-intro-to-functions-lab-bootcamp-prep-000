d96a0024d38a3ad60184d87d946bc0945fd65631
Nick Bradley <nick.bradley.193@pm.me>
2018-04-12 02:59:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase();
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase());
+}
+
+function logwhisper(string) {
+  console.log(string.toLowerCase());
+}
+
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase()) {
+    return "I can't hear you!";
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED"
+  }
+}
